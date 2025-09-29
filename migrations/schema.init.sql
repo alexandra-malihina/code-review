2cd386dcbfebe50a7e8cb1f8073dd7da7d4b4511
AlMa <work.mal@mail.ru>
2025-09-29 16:31:14 +0400

    3 column is_active change comment

--- migrations/schema.init.sql
+++ migrations/schema.init.sql
@@ -3,7 +3,7 @@ create table if not exists products
     id int auto_increment primary key,
     uuid  varchar(255) not null comment 'UUID товара',
     category  varchar(255) not null comment 'Категория товара',
-    is_active tinyint default 1  not null comment 'Флаг активности',
+    is_active tinyint default 1  not null comment 'Флаг активности (1 - активно, 0 - не активно)',
     name varchar(255) default '' not null comment 'Наименование товара',
     description text null comment 'Описание товара',
     thumbnail  varchar(255) null comment 'Ссылка на картинку',
